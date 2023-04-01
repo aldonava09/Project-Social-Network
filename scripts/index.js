d31ed4c277cb99212655e21594af4d6f1b6e036c
aldonava09 <aldonavargas@gmail.com>
2023-03-31 20:18:52 -0600

    Agregar Cards/Forms

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,28 @@
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.profile__edit-form-close-button');
-let profileContainer = document.querySelector('.profile__edit-form-container_hidden');
-let overlay = document.querySelector('.overlay_hidden');
+const editProfileButton = document.querySelector('.profile__edit-button');
+const closeProfileButton = document.querySelector('.profile__edit-form-close-button');
+const profileFormContainer = document.querySelector('.profile__edit-form-container_hidden');
+const overlay = document.querySelector('.overlay_hidden');
 
 function editProfileOpen() {
 
-    profileContainer.classList.toggle('profile__edit-form-container');
+    profileFormContainer.classList.toggle('profile__edit-form-container');
     overlay.classList.toggle('overlay');
 
 }
 
-editButton.addEventListener("click" , editProfileOpen);
-closeButton.addEventListener("click" , editProfileOpen);
-overlay.addEventListener("click" , editProfileOpen);
+editProfileButton.addEventListener("click" , editProfileOpen);
+closeProfileButton.addEventListener("click" , editProfileOpen);
 
-let profileName = document.querySelector('.profile__name');
-let profileProfession = document.querySelector('.profile__profession');
-let profileNameInput = document.querySelector('.profile__input-name');
-let profileProfessionInput = document.querySelector('.profile__input-profession');
+const profileName = document.querySelector('.profile__name');
+const profileProfession = document.querySelector('.profile__profession');
+const profileNameInput = document.querySelector('.profile__input-name');
+const profileProfessionInput = document.querySelector('.profile__input-profession');
 
 
 profileName.textContent = "Aldo Navarro"
 profileProfession.textContent = "Programador"
 
-let profileSubmitButton = document.querySelector('.profile__submit-button');
+const profileSubmitButton = document.querySelector('.profile__submit-button');
 
 
 function editProfaileValues(evt) {
@@ -31,9 +30,56 @@ function editProfaileValues(evt) {
     evt.preventDefault();
     profileName.textContent = profileNameInput.value;
     profileProfession.textContent = profileProfessionInput.value;
-    profileContainer.classList.toggle('profile__edit-form-container');
+    profileFormContainer.classList.toggle('profile__edit-form-container');
     overlay.classList.toggle('overlay');
 
 }
 
 profileSubmitButton.addEventListener("click" , editProfaileValues);
+
+const addNewImgButton = document.querySelector('.profile__add-button-container');
+const closeNewPlaceButton = document.querySelector('.profile__new-place-form-close-button');
+const newPlaceFormContainer = document.querySelector('.profile__new-place-form-container_hidden');
+
+function addNewImgOpen() {
+
+    newPlaceFormContainer.classList.toggle('profile__new-place-form-container');
+    overlay.classList.toggle('overlay');
+
+}  
+
+addNewImgButton.addEventListener("click", addNewImgOpen);
+closeNewPlaceButton.addEventListener("click", addNewImgOpen);
+const cardTitleInput = document.querySelector('.profile__new-place-input-title');
+const cardLinkInput = document.querySelector('.profile__new-place-input-image-link');
+const cards = document.querySelector(".cards");
+const cardTemplate = document.querySelector("#card-template").content;
+const card = cardTemplate.querySelector(".cards__card").cloneNode(true);
+
+const initialCards = [
+    {
+      name: "Nueva York, Nueva York",
+      link: "../"
+    },
+    {
+      name: "Lago Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+    },
+    {
+      name: "Montañas Calvas",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+    },
+    {
+      name: "Parque Nacional de la Vanoise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
+    }
+  ]; 
+
